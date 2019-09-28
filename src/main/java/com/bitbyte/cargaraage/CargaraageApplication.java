5fb7508144595df5d85dffc947b6fea254aa180f
Sai Reddy <sai.reddy@target.com>
2019-09-27 22:28:11 -0500

    Initial commit

---
+++ src/main/java/com/bitbyte/cargaraage/CargaraageApplication.java
@@ -0,0 +1,13 @@
+package com.bitbyte.cargaraage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaraageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargaraageApplication.class, args);
+    }
+
+}
